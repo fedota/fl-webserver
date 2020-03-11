f4c60367a6c526d5008f3add6531b95b58c3d512
Praveen Gupta <pvgupta24@gmail.com>
2020-03-11 12:47:57 +0530

    Fixes issues with updating yaml for creating PV

--- fedota/kube.py
+++ fedota/kube.py
@@ -5,83 +5,91 @@ import os
 from django.conf import settings
 
 from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 
 def create_namespace(name):
     body = client.V1Namespace()
     body.metadata = client.V1ObjectMeta(name=name)
     core_v1 = client.CoreV1Api()
     resp = core_v1.create_namespace(body=body)
-    print("Namespace created. status='%s'" % resp.metadata.name)
+    print("Namespace created. status='%s'" % resp)
 
 def delete_namespace(name):
     core_v1 = client.CoreV1Api()
-    resp = core_v1.delete_namespace(name=name, body=client.V1DeleteOptions())
-    print("Namespace deleted. status='%s'" % resp.metadata.name)
+    try:
+        resp = core_v1.delete_namespace(name=name, body=client.V1DeleteOptions())
+        print("Namespace deleted. status='%s'" % resp)
+    except ApiException:
+        print("Namespace not found")
 
-
-def spawn_services(id):
-    config.load_kube_config()
-
+def create_service_deployment(id, service_name, deployment_config_file):
     core_v1 = client.CoreV1Api()
     apps_v1 = client.AppsV1Api()
 
+    # create fl coordinator/selector service and deployment
+    with open(deployment_config_file) as f:
+        dep = yaml.safe_load_all(f)
+
+        for yaml_object in dep:
+            print(yaml_object['kind'])
+            if yaml_object['kind'] == 'Service':
+                try:
+                    resp = core_v1.create_namespaced_service(
+                        namespace=id, body=yaml_object)
+                    print(("FL {} service created. Status={}").format(service_name, resp.metadata.name))
+                except ApiException as e:
+                    print(e.body)
+
+            elif yaml_object['kind'] == 'Deployment':
+                try:
+                    resp = apps_v1.create_namespaced_deployment(
+                        namespace=id, body=yaml_object)
+                    print(("FL {} deployment created. Status={}").format(service_name, resp.metadata.name))
+                except ApiException as e:
+                    print(e.body)
+
+def spawn_services(id):
+    config.load_kube_config()
+    core_v1 = client.CoreV1Api()
+
     # create the namespace as the fl problem id
-    if str(id) in core_v1.list_namespace().items:
-        delete_namespace(str(id))
-    create_namespace(str(id))
+    try:
+        api_response = core_v1.read_namespace(str(id))
+    except ApiException:
+        create_namespace(str(id))
 
     # create a fl pv for each problem
     pv_config_file = os.path.join(
         settings.BASE_DIR, "k8s/fl-pv.yaml")
     with open(pv_config_file) as f:
-        dep = yaml.safe_load_all(f)
-        dep['metadata'].name = str(id)
-        dep['spec'].hostPath.path = os.path.join(
+        dep =yaml.safe_load(f)
+        dep['metadata']['name'] = str(id)
+        dep['spec']['hostPath']['path'] = os.path.join(
         settings.BASE_DIR, str(id))
-        resp = core_v1.create_persistent_volume(body=dep)
-        print("FL PV created. status='%s'" % resp.metadata.name)
+        try:
+            resp = core_v1.create_persistent_volume(body=dep)
+            print("FL PV created. status='%s'" % resp.metadata.name)
+        except ApiException as e:
+            # TODO: Handle AlreadyExists error
+            print(e.body)
 
     # create fl coordinator service and deployment
     pvc_config_file = os.path.join(
         settings.BASE_DIR, "k8s/fl-pvc.yaml")
     with open(pvc_config_file) as f:
-        dep = yaml.safe_load_all(f)
-        resp = core_v1.create_namespaced_persistent_volume_claim(
-            namespace=id, body=dep)
-        print("FL PVC created. status='%s'" % resp.metadata.name)
+        dep = yaml.safe_load(f)
+        try:
+            resp = core_v1.create_namespaced_persistent_volume_claim(
+                namespace=id, body=dep)
+            print("FL PVC created. status='%s'" % resp.metadata.name)
+        except ApiException as e:
+            print(e.body)
 
-    # create fl coordinator service and deployment
     deployment_config_file = os.path.join(
         settings.BASE_DIR, "k8s/fl-coordinator.yaml")
-    with open(deployment_config_file) as f:
-        dep = yaml.safe_load_all(f)
-
-        for i in dep:
-            print(i['kind'])
-            if i['kind'] == 'Service':
-                resp = core_v1.create_namespaced_service(
-                    namespace=id, body=i)
-                print("FL Coorinator Service created. status='%s'" % resp.metadata.name)
-
-            elif i['kind'] == 'Deployment':
-                resp = apps_v1.create_namespaced_deployment(
-                    namespace=id, body=i)
-                print("FL Coordinator Deployment created. status='%s'" % resp.metadata.name)
+    create_service_deployment(id, "Co-ordinator", deployment_config_file)
 
     # create fl selector service and deployment
     deployment_config_file = os.path.join(
-        settings.BASE_DIR, "k8s/fl-selector.yaml")
-    with open(deployment_config_file) as f:
-        dep = yaml.safe_load_all(f)
-
-        for i in dep:
-            print(i['kind'])
-            if i['kind'] == 'Service':
-                resp = core_v1.create_namespaced_service(
-                    namespace=id, body=i)
-                print("FL Selector Service created. status='%s'" % resp.metadata.name)
-
-            elif i['kind'] == 'Deployment':
-                resp = apps_v1.create_namespaced_deployment(
-                    namespace=id, body=i)
-                print("FL Selector Deployment created. status='%s'" % resp.metadata.name)
+        settings.BASE_DIR, "k8s/fl-selector.yaml")    
+    create_service_deployment(id, "Selector", deployment_config_file)
